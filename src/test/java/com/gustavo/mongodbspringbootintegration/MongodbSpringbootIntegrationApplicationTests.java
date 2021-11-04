9ab7fd89c9ccb7cf7f0f37fb305a522e4ab08851
GuhTiagoSilva <gustavo.tiago.gts@outlook.com>
2021-11-03 21:28:59 -0300

    Initial Commit

---
+++ src/test/java/com/gustavo/mongodbspringbootintegration/MongodbSpringbootIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavo.mongodbspringbootintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbSpringbootIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
